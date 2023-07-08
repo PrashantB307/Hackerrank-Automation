83a1170d99000aaf1dddbb658cee1088feca8dc9
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-07-09 00:28:53 +0530

    9-7-2023

--- HackerRank.js
+++ HackerRank.js
@@ -1,3 +1,8 @@
 // Use ===> node HackerRank.js  ( for run the code in terminal )
 // Use ===>  npm install puppeteer  ( for install puppeteer )
 
+const puppeteer = require("puppeteer");
+
+let { email, password } = require('./secrets');
+let { answer } = require("./codes"); 
+
