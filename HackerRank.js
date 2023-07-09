c4230057b6209b5e5a2340b41ec56d4e73978b97
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-07-10 00:18:00 +0530

    10-7-2023

--- HackerRank.js
+++ HackerRank.js
@@ -15,3 +15,11 @@ let browserOpenPromise = puppeteer.launch({
    // executablePath:"C:\Program Files\Google\Chrome\Application\chrome.exe"
 });
 
+browserOpenPromise.then(function(browser) {
+    console.log("Browser is open");
+    console.log(browser);
+    //An array of all open paes inside the Browser
+    //returns an array with all the pages in all browser contexts
+    let alltabsPromise = browser.pages();
+    return alltabsPromise;
+})
