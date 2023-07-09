73f99fb04598a69cffd0308e9e1c313d1daad6c2
Prashant Bhardwaj <pb28.rkt@gmail.com>
2023-07-10 00:17:30 +0530

    10-7-2023

--- HackerRank.js
+++ HackerRank.js
@@ -6,3 +6,12 @@ const puppeteer = require("puppeteer");
 let { email, password } = require('./secrets');
 let { answer } = require("./codes"); 
 
+let cTab;
+let browserOpenPromise = puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--start-maximized"],
+    //chrome://version/
+   // executablePath:"C:\Program Files\Google\Chrome\Application\chrome.exe"
+});
+
